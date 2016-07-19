b4b483c427f2a8454f70f6a47e9607ca8b3c63ba
Mark Noneman <mark@agilitysw.com>
2016-07-19 19:26:26 -0400

    Initial commit to repository.

---
+++ src/test/java/helpers/Path.java
@@ -0,0 +1,13 @@
+/*
+ * Authors: Rainer Grau, Daniel Tobler, Zuehlke Technology Group
+ * Copyright (c) 2013 All Right Reserved
+ */ 
+
+package helpers;
+
+public class Path {
+
+	public static String Combine(String path1, String path2) {
+		return path1 + "\\" + path2;
+	}
+}
